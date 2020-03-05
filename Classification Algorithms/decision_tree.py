23dfdf969b6d64e9305829f559c24db9210c3c87
Sarath <49370420+sarath1398@users.noreply.github.com>
2020-03-05 09:19:42 +0530

    Update decision_tree.py
    Train and test splitted in 70-30 ratio and data is shuffled for better accuracy (Here random_state=2 gives more accuracy till now! )

--- Classification Algorithms/decision_tree.py
+++ Classification Algorithms/decision_tree.py
@@ -6,49 +6,47 @@ from sklearn.tree import BaseDecisionTree
 from sklearn.tree import ExtraTreeClassifier
 from sklearn.tree import ExtraTreeRegressor
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import GridSearchCV,train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 
-fitdata= pd.read_excel(r"C:\Users\Sarath\Desktop\DS Hackathon\Files\UCI Repository\fitdata.xlsx")
-predictdata = pd.read_excel(r"C:\Users\Sarath\Desktop\DS Hackathon\Files\UCI Repository\predictiondata.xlsx")
-fitdata= np.asarray(fitdata)
-predictdata=np.asarray(predictdata)
-X = fitdata[:,0:13]
-Y = fitdata[:,13]
-x=predictdata[:,0:13]
-y=predictdata[:,13]
-scaler=MinMaxScaler(feature_range=(0,1))
-X=scaler.fit_transform(X)
-x=scaler.fit_transform(x)
+alldata = pd.read_csv(r'C:\Users\Sarath\Desktop\DS Hackathon\Files\UCI Repository\heart.csv')
+alldata = np.asarray(alldata)
+X = alldata[:, 0:13]
+y = alldata[:, 13:]
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
 np.set_printoptions(precision=3)
 
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.3, random_state=2)
+
 tree=DecisionTreeClassifier()
-tree=tree.fit(X,Y)
-Y1=tree.predict(X)
-Y2=tree.predict(x)
-print(accuracy_score(Y,Y1)) 
-print(accuracy_score(y,Y2)) 
+tree=tree.fit(X_train,Y_train)
+Y1=tree.predict(X_train)
+Y2=tree.predict(X_test)
+print(accuracy_score(Y_train,Y1)) 
+print(accuracy_score(Y_test,Y2)) 
 
 tree=DecisionTreeRegressor()
-tree=tree.fit(X,Y)
-Y1=tree.predict(X)
-Y2 = tree.predict(x)
-print(accuracy_score(Y, Y1))  # 100% Accuracy again..There's something definitely wrong with the code!
-print(accuracy_score(y, Y2))
+tree = tree.fit(X_train, Y_train)
+Y1 = tree.predict(X_train)
+Y2 = tree.predict(X_test)
+print(accuracy_score(Y_train, Y1))
+print(accuracy_score(Y_test, Y2))
 
 tree=ExtraTreeClassifier()
-tree = tree.fit(X, Y)
-Y1 = tree.predict(X)
-Y2 = tree.predict(x)
-print(accuracy_score(Y, Y1)) #Hattrick
-print(accuracy_score(y, Y2))
+tree = tree.fit(X_train, Y_train)
+Y1 = tree.predict(X_train)
+Y2 = tree.predict(X_test)
+print(accuracy_score(Y_train, Y1))
+print(accuracy_score(Y_test, Y2))
 
 tree=ExtraTreeRegressor()
-tree = tree.fit(X, Y)
-Y1 = tree.predict(X)
-Y2 = tree.predict(x)
-print(accuracy_score(Y, Y1)) #Hattrick+1
-print(accuracy_score(y, Y2))
+tree = tree.fit(X_train, Y_train)
+Y1 = tree.predict(X_train)
+Y2 = tree.predict(X_test)
+print(accuracy_score(Y_train, Y1))
+print(accuracy_score(Y_test, Y2))
 '''
 tree=BaseDecisionTree()
 tree = tree.fit(x, y)
