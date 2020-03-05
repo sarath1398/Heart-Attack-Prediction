74f0a5f344eb5479a847e207ee5a7077a5b0cdbb
Sarath <49370420+sarath1398@users.noreply.github.com>
2020-03-05 09:40:41 +0530

    Updated file

--- Classification Algorithms/support_vector_machine.py
+++ Classification Algorithms/support_vector_machine.py
@@ -7,30 +7,27 @@ from sklearn.svm import NuSVR
 from sklearn.svm import LinearSVC
 from sklearn.svm import LinearSVR
 from sklearn.svm import OneClassSVM
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 
-fitdata = pd.read_excel(r"C:\Users\Sarath\Desktop\DS Hackathon\Files\UCI Repository\fitdata.xlsx")
-predictdata = pd.read_excel(r"C:\Users\Sarath\Desktop\DS Hackathon\Files\UCI Repository\predictiondata.xlsx")
-fitdata = np.asarray(fitdata)
-predictdata = np.asarray(predictdata)
-X = fitdata[:, 0:13]
-Y = fitdata[:, 13]
-x = predictdata[:, 0:13]
-y = predictdata[:, 13]
-scaler = MinMaxScaler(feature_range=(0, 1))
+alldata = pd.read_csv(r'C:\Users\Sarath\Desktop\DS Hackathon\Files\UCI Repository\heart.csv')
+alldata = np.asarray(alldata)
+X = alldata[:, 0:13]
+y = alldata[:, 13:]
+scaler = MinMaxScaler()
 X = scaler.fit_transform(X)
-x = scaler.fit_transform(x)
 np.set_printoptions(precision=3)
 
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.3, random_state=2)
+
 tree = SVC()
-tree = tree.fit(X, Y)
-Y1 = tree.predict(X)
-Y2 = tree.predict(x)
-# Has range between 83 and 87 with a max value of of 87.7%
-print(accuracy_score(Y, Y1))
-print(accuracy_score(y, Y2))
+tree = tree.fit(X_train, Y_train)
+Y1 = tree.predict(X_train) 
+Y2 = tree.predict(X_test)
+print(accuracy_score(Y_train, Y1)) #0.8820754716981132
+print(accuracy_score(Y_test, Y2)) #0.8901098901098901
 '''
 tree = SVR()
 tree = tree.fit(x, y)
@@ -38,12 +35,11 @@ y1 = tree.predict(x)
 print(accuracy_score(y, y1))
 '''
 tree = NuSVC()
-tree = tree.fit(X, Y)
-Y1 = tree.predict(X)
-Y2 = tree.predict(x)
-# Has range between 83 and 87 with a max value of of 87.7%
-print(accuracy_score(Y, Y1))
-print(accuracy_score(y, Y2))
+tree = tree.fit(X_train, Y_train)
+Y1 = tree.predict(X_train)
+Y2 = tree.predict(X_test)
+print(accuracy_score(Y_train, Y1)) #0.8820754716981132
+print(accuracy_score(Y_test, Y2)) #0.8901098901098901
 '''
 tree = NuSVR()
 tree = tree.fit(x, y)
@@ -51,23 +47,21 @@ y1 = tree.predict(x)
 print(accuracy_score(y, y1))
 '''
 tree = LinearSVC()
-tree = tree.fit(X, Y)
-Y1 = tree.predict(X)
-Y2 = tree.predict(x)
-# Has range between 83 and 87 with a max value of of 87.7%
-print(accuracy_score(Y, Y1))
-print(accuracy_score(y, Y2))
+tree = tree.fit(X_train, Y_train)
+Y1 = tree.predict(X_train)
+Y2 = tree.predict(X_test)
+print(accuracy_score(Y_train, Y1)) #0.8443396226415094
+print(accuracy_score(Y_test, Y2)) #0.8681318681318682
 '''
 tree = LinearSVR()
 tree = tree.fit(x, y)
 y1 = tree.predict(x)
 print(accuracy_score(y, y1))
-'''
-tree = OneClassSVM()
-tree = tree.fit(X, Y)
-Y1 = tree.predict(X)
-Y2 = tree.predict(x)
-# Has range between 83 and 87 with a max value of of 87.7%
-print(accuracy_score(Y, Y1))
-print(accuracy_score(y, Y2))
 
+tree = OneClassSVM()
+tree = tree.fit(X_train, Y_train)
+Y1 = tree.predict(X_train)
+Y2 = tree.predict(X_test)
+print(accuracy_score(Y_train, Y1))
+print(accuracy_score(Y_test, Y2))
+'''
