9d9173f957b4e86b5f83cd5499fe8701aa5b34c3
Sarath <49370420+sarath1398@users.noreply.github.com>
2020-03-05 10:28:42 +0530

    Updated Naive Bayes

--- Classification Algorithms/naive_bayes.py
+++ Classification Algorithms/naive_bayes.py
@@ -10,89 +10,52 @@ from sklearn.naive_bayes import _BaseNB
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
 
-fitdata = pd.read_excel(r"C:\Users\Sarath\Desktop\DS Hackathon\Files\UCI Repository\fitdata.xlsx")
-predictdata = pd.read_excel(r"C:\Users\Sarath\Desktop\DS Hackathon\Files\UCI Repository\predictiondata.xlsx")
-fitdata = np.asarray(fitdata)
-predictdata = np.asarray(predictdata)
-X = fitdata[:, 0:13]
-Y = fitdata[:, 13]
-x = predictdata[:, 0:13]
-y = predictdata[:, 13]
-scaler = MinMaxScaler(feature_range=(0, 1))
+alldata = pd.read_csv(r'C:\Users\Sarath\Desktop\DS Hackathon\Files\UCI Repository\heart.csv')
+alldata = np.asarray(alldata)
+X = alldata[:, 0:13]
+y = alldata[:, 13:]
+scaler = MinMaxScaler()
 X = scaler.fit_transform(X)
-x = scaler.fit_transform(x)
 np.set_printoptions(precision=3)
 
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.3, random_state=14)
 
 #BernoulliNB
-r = BernoulliNB()
-r.fit(X, Y)
-Y1 = r.predict(X)
-Y2 = r.predict(x)
-print(accuracy_score(Y, Y1)) # 83.8% Accuracy
-print( accuracy_score(y, Y2))  # 83.8% Accuracy
+
+nvb = BernoulliNB()
+nvb = nvb.fit(X_train, Y_train)
+Y1 = nvb.predict(X_train)
+Y2 = nvb.predict(X_test)
+print(accuracy_score(Y_train, Y1)) #0.8490566037735849
+print(accuracy_score(Y_test, Y2)) #0.8131868131868132
 
 
 #ComplementNB
 
-r = ComplementNB()
-r.fit(X, Y)
-Y1 = r.predict(X)
-Y2 = r.predict(x)
-print(accuracy_score(Y, Y1))  
-print(accuracy_score(y, Y2))  
-'''
-#CategoricalNB
+nvb = ComplementNB()
+nvb = nvb.fit(X_train, Y_train)
+Y1 = nvb.predict(X_train)
+Y2 = nvb.predict(X_test)
+print(accuracy_score(Y_train, Y1)) #0.8018867924528302
+print(accuracy_score(Y_test, Y2)) #0.7912087912087912
 
-r = CategoricalNB()
-r.fit(X, Y)
-Y1 = r.predict(X)
-Y2 = r.predict(x)
-print(accuracy_score(Y, Y1))  # 83.8% Accuracy
-print(accuracy_score(y, Y2))  # 83.8% Accuracy
-'''
-'''
-#BaseDiscreteNB
-r = BaseDiscreteNB()
-r.fit(x, y)
-y1 = r.predict(x)
-print("Base Discrete NB: ", accuracy_score(y, y1))  
-
-#BaseNB
-r = BaseNB()
-r.fit(x, y)
-y1 = r.predict(x)
-print("Base NB: ", accuracy_score(y, y1))
-
-#_BaseDiscreteNB
-r =_BaseDiscreteNB()
-r.fit(x, y)
-y1 = r.predict(x)
-print("_Base Discrete NB: ", accuracy_score(y, y1))
-
-#_BaseNB
-r = _BaseNB()
-r.fit(x, y)
-y1 = r.predict(x)
-print("_Base NB: ", accuracy_score(y, y1))
-'''
 #MultinomialNB
 
-r = MultinomialNB()
-r.fit(X, Y)
-Y1 = r.predict(X)
-Y2 = r.predict(x)
-print(accuracy_score(Y, Y1))  
-print(accuracy_score(y, Y2))  
+nvb = MultinomialNB()
+nvb = nvb.fit(X_train, Y_train)
+Y1 = nvb.predict(X_train)
+Y2 = nvb.predict(X_test)
+print(accuracy_score(Y_train, Y1)) #0.8113207547169812
+print(accuracy_score(Y_test, Y2)) #0.7692307692307693
 
 #GaussianNB
 
-r = GaussianNB()
-r.fit(X, Y)
-Y1 = r.predict(X)
-Y2 = r.predict(x)
-print(accuracy_score(Y, Y1))  # 83.8% Accuracy
-print(accuracy_score(y, Y2))  # 83.8% Accuracy
+nvb = GaussianNB()
+nvb = nvb.fit(X_train, Y_train)
+Y1 = nvb.predict(X_train)
+Y2 = nvb.predict(X_test) 
+print(accuracy_score(Y_train, Y1)) #0.8490566037735849
+print(accuracy_score(Y_test, Y2)) #0.8241758241758241
