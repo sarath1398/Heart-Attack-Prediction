490a93c310ecf2e64eb8649f7dbdced9697ae3ee
Sarath <49370420+sarath1398@users.noreply.github.com>
2020-03-05 09:58:44 +0530

    Updated Random Forest

--- Classification Algorithms/random_forest.py
+++ Classification Algorithms/random_forest.py
@@ -7,113 +7,53 @@ from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import StackingClassifier
 from sklearn.ensemble import VotingClassifier
-from sklearn.ensemble import AdaBoostRegressor
-from sklearn.ensemble import BaggingRegressor
-from sklearn.ensemble import ExtraTreesRegressor
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.ensemble import StackingRegressor
-from sklearn.ensemble import VotingRegressor
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import RandomTreesEmbedding
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 
-fitdata = pd.read_excel(r"C:\Users\Sarath\Desktop\DS Hackathon\Files\UCI Repository\fitdata.xlsx")
-predictdata = pd.read_excel(r"C:\Users\Sarath\Desktop\DS Hackathon\Files\UCI Repository\predictiondata.xlsx")
-fitdata = np.asarray(fitdata)
-predictdata = np.asarray(predictdata)
-X = fitdata[:, 0:13]
-Y = fitdata[:, 13]
-x = predictdata[:, 0:13]
-y = predictdata[:, 13]
-scaler = MinMaxScaler(feature_range=(0, 1))
+alldata = pd.read_csv(r'C:\Users\Sarath\Desktop\DS Hackathon\Files\UCI Repository\heart.csv')
+alldata = np.asarray(alldata)
+X = alldata[:, 0:13]
+y = alldata[:, 13:]
+scaler = MinMaxScaler()
 X = scaler.fit_transform(X)
-x = scaler.fit_transform(x)
 np.set_printoptions(precision=3)
 
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.3, random_state=2)
+
 tree = AdaBoostClassifier()
-tree = tree.fit(X, Y)
-Y1 = tree.predict(X)
-Y2 = tree.predict(x)
-# Has range between 83 and 87 with a max value of of 87.7%
-print(accuracy_score(Y, Y1))
-print(accuracy_score(y, Y2))
+tree = tree.fit(X_train, Y_train)
+Y1 = tree.predict(X_train)
+Y2 = tree.predict(X_test)
+print(accuracy_score(Y_train, Y1)) #0.9386792452830188
+print(accuracy_score(Y_test, Y2)) #0.8681318681318682
 
 tree = BaggingClassifier()
-tree = tree.fit(X, Y)
-Y1 = tree.predict(X)
-Y2 = tree.predict(x)
-# Has range between 83 and 87 with a max value of of 87.7%
-print(accuracy_score(Y, Y1))
-print(accuracy_score(y, Y2))
+tree = tree.fit(X_train, Y_train)
+Y1 = tree.predict(X_train)
+Y2 = tree.predict(X_test)
+print(accuracy_score(Y_train, Y1)) #0.9952830188679245
+print(accuracy_score(Y_test, Y2)) #0.8461538461538461
 
 tree = ExtraTreesClassifier()
-tree = tree.fit(X, Y)
-Y1 = tree.predict(X)
-Y2 = tree.predict(x)
-# Has range between 83 and 87 with a max value of of 87.7%
-print(accuracy_score(Y, Y1))
-print(accuracy_score(y, Y2))
+tree = tree.fit(X_train, Y_train)
+Y1 = tree.predict(X_train)
+Y2 = tree.predict(X_test)
+print(accuracy_score(Y_train, Y1)) #1.0
+print(accuracy_score(Y_test, Y2)) #0.8791208791208791
 
 tree = GradientBoostingClassifier()
-tree = tree.fit(X, Y)
-Y1 = tree.predict(X)
-Y2 = tree.predict(x)
-# Has range between 83 and 87 with a max value of of 87.7%
-print(accuracy_score(Y, Y1))
-print(accuracy_score(y, Y2))
+tree = tree.fit(X_train, Y_train)
+Y1 = tree.predict(X_train)
+Y2 = tree.predict(X_test)
+print(accuracy_score(Y_train, Y1)) #0.9952830188679245
+print(accuracy_score(Y_test, Y2)) #0.8791208791208791
 
 tree = RandomForestClassifier() 
-tree = tree.fit(X, Y)
-Y1 = tree.predict(X)
-Y2 = tree.predict(x)
-# Has range between 83 and 87 with a max value of of 87.7%
-print(accuracy_score(Y, Y1))
-print(accuracy_score(y, Y2))
-'''
-tree = StackingClassifier()
-tree = tree.fit(x, y)
-y1 = tree.predict(x)
-print(accuracy_score(y, y1))
-
-tree = VotingClassifier()
-tree = tree.fit(x, y)
-y1 = tree.predict(x)
-print(accuracy_score(y, y1))
-
-tree = AdaBoostRegressor()
-tree = tree.fit(x, y)
-y1 = tree.predict(x)
-print(accuracy_score(y, y1))
-
-tree = BaggingRegressor()
-tree = tree.fit(x, y)
-y1 = tree.predict(x)
-print(accuracy_score(y, y1))
-
-tree = ExtraTreesRegressor()
-tree = tree.fit(x, y)
-y1 = tree.predict(x)
-print(accuracy_score(y, y1))
-
-tree = GradientBoostingRegressor()
-tree = tree.fit(x, y)
-y1 = tree.predict(x)
-print(accuracy_score(y, y1))
-
-tree = RandomForestRegressor()
-tree = tree.fit(x, y)
-y1 = tree.predict(x)
-print(accuracy_score(y, y1))
-
-tree = StackingRegressor()
-tree = tree.fit(x, y)
-y1 = tree.predict(x)
-print(accuracy_score(y, y1))
-
-tree = VotingRegressor()
-tree = tree.fit(x, y)
-y1 = tree.predict(x)
-print(accuracy_score(y, y1))
-'''
+tree = tree.fit(X_train, Y_train)
+Y1 = tree.predict(X_train)
+Y2 = tree.predict(X_test)
+print(accuracy_score(Y_train, Y1)) #1.0
+print(accuracy_score(Y_test, Y2)) #0.8791208791208791
